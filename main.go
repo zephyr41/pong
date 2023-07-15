afdf80dc7d4f242f4670e5f99156647236ef397d
Zephyr <nils.jaudon@ynov.com>
2023-07-15 15:16:50 +0200

    HI Moove

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/gdamore/tcell/v2" // permet de crée une interface utlisateur
 )
@@ -16,27 +17,34 @@ func main() {
 	if err := screen.Init(); err != nil {
 		log.Fatalf("could not init screen: %v", err)
 	}
-	defStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorBlack)
+	defStyle := tcell.StyleDefault.Background(tcell.ColorPaleTurquoise).Foreground(tcell.ColorBlack)
 	screen.SetStyle(defStyle)
+
+	go Run(screen, defStyle)
+
+	for {
+		switch ev := screen.PollEvent().(type) {
+		case *tcell.EventResize:
+			screen.Sync()
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+				screen.Fini()
+				os.Exit(0)
+			}
+
+		}
+	}
+}
+func Run(screen tcell.Screen, defStyle tcell.Style) {
 	x := 0
 	for {
-
-		screen.SetContent(x, 10, 'H', nil, defStyle)
+		screen.Clear()
+		screen.SetContent(x,10, 'H', nil, defStyle)
 		screen.SetContent(x+1, 10, 'i', nil, defStyle)
 		screen.SetContent(x+2, 10, '!', nil, defStyle)
 
 		screen.Show()
+		time.Sleep(40 * time.Millisecond)
 		x++
-		switch event := screen.PollEvent().(type) {
-		case *tcell.EventResize:
-			screen.Sync()
-		case *tcell.EventKey:
-			if event.Key() == tcell.KeyEscape || event.Key() == tcell.KeyCtrlC {
-				screen.Fini()
-				os.Exit(0)
-			}
-		}
-
 	}
-
 }
